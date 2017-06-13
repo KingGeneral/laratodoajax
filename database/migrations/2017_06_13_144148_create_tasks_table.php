a151b6a1da62178016350a216a0c470c5e9b607b
KingGeneraL <zone.nano09@gmail.com>
2017-06-13 23:13:10 +0700

    [Update] fixing issue class task not found
    > add use App\Task;
    - in migration of create_task_table
    - now use this $tasks = \App\Task::all();
    - - before using this one >>>>>> $tasks = Task::all();
    
    > fixing boolean issue in jquery when updates
    > add new welcome blade + boolean
    
    > prevent Call to undefined method Illuminate\Support\Facades\Request::all()
    - use Illuminate\Http\Request;

--- database/migrations/2017_06_13_144148_create_tasks_table.php
+++ database/migrations/2017_06_13_144148_create_tasks_table.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+use App\Task;
+
 class CreateTasksTable extends Migration
 {
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+//prevent Call to undefined method Illuminate\Support\Facades\Request::all()
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +14,49 @@
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//index
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = \App\Task::all();
+
+    return View::make('welcome')->with('tasks',$tasks);
+});
+
+//get id
+Route::get('/tasks/{task_id?}',function($task_id){
+    $task = \App\Task::find($task_id);
+
+    return Response::json($task);
+});
+
+// post
+Route::post('/tasks',function(Request $request){
+    $task = \App\Task::create($request->all());
+
+    return Response::json($task);
+});
+
+//store
+Route::put('/tasks/{task_id?}',function(Request $request,$task_id){
+
+	// dd($request);
+    $task = \App\Task::find($task_id);
+
+    $task->task = $request->task;
+    $task->description = $request->description;
+    $task->done = $request->done;
+
+    $task->save();
+
+    return Response::json($task);
+});
+
+//delete
+Route::delete('/tasks/{task_id?}',function($task_id){
+    $task = \App\Task::destroy($task_id);
+
+    return Response::json($task);
 });
